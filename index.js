54ea2b70668e3354ddc3a64f602c9bb31ef041d8
login <74485431+larkovsasha@users.noreply.github.com>
2022-02-17 21:23:46 +0300

    test

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+fs = require('fs')
+const PORT = 9999
+
+app.get('/', (req, res) => {
+    const html = '<a>https://gentle-chamber-10032.herokuapp.com/html/Gepard</a>'
+    res.end(html)
+})
+app.get('/html/*', (req, res) => {
+    const html = fs.readFileSync('./index.html')
+    res.end(html)
+})
+app.get('/index.css', (req, res) => {
+    const css = fs.readFileSync('./index.css')
+    res.end(css)
+})
+app.get('/script.js', (req, res) => {
+    const js = fs.readFileSync('./script.js')
+    res.end(js)
+})
+
+// app.get('/rhino', (req, res) => {
+//     const html = fs.readFileSync('./index.html')
+//     res.end(html)
+// })
+
+    
+
+app.get('/assets/best_music.mp3', (req, res) => {
+    // res.set({'Content-Type': 'json'});
+    // let readStream = fs.createReadStream('./assets/best_music.mp3');
+    // readStream.pipe(res);
+    try {
+        const mp3 = fs.readFileSync('./assets/best_music.mp3')
+        res.set({
+            "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+            'accept-ranges': 'bytes',
+            'Content-Length': '163584',
+            'Content-Range': 'bytes 0-163583/163584',
+            'content-type': 'audio/mpeg'
+        }).status(206).sendFile('G:\\dev\\school project\\assets\\best_music.mp3')
+    }catch (e){
+        console.log(e)
+    }
+})
+
+
+
+
+app.get('/about', (req, res) => {
+    res.end('<h1>about<h1/>')
+})
+
+
+
+app.listen(PORT, () => {
+    console.log('server has been started')
+})
