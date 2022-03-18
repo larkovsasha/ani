27d7579bcebdc2d35cab9b4329cb862971633e2c
login <74485431+larkovsasha@users.noreply.github.com>
2022-03-18 21:21:18 +0300

    dins

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 fs = require('fs')
 const PORT = process.env.PORT || 8080
 
+app.use(express.static('static'));
+
 app.get('https://school-project2.herokuapp.com/', (req, res) => {
     const html = '<a>https://gentle-chamber-10032.herokuapp.com/html/Gepard</a>'
     res.end(html)
@@ -19,11 +21,6 @@ app.get('/script.js', (req, res) => {
     const js = fs.readFileSync('./script.js')
     res.end(js)
 })
-
-// app.get('/rhino', (req, res) => {
-//     const html = fs.readFileSync('./index.html')
-//     res.end(html)
-// })
 
     
 
